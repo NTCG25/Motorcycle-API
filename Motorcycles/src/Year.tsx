640a48f4464533210907356ffc80b2a6d2ec9fa9
NTCG25 <nigelmar2002@gmail.com>
2023-03-15 11:14:49 -0500

    New Year Module

---
+++ Motorcycles/src/Year.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+    function Year () {
+    return (
+        <div>
+            <h2> Year: </h2>
+            <input></input>
+        </div>
+        
+    )
+}
+
+export default Year 
